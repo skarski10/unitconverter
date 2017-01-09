2c0d6e5829d4f205a6e04b91f552bec283b09a7a
Kory Skarbek and Melvin Gruschow <kory@>
2017-01-09 13:25:28 -0800

    add the homework with basic jquery linking and doc ready.

--- js/unitConverter.js
+++ js/unitConverter.js
@@ -1,10 +1,10 @@
-var converter = function(oz){
+var ozToLbs = function(oz){
   return (oz * (1/16));
 }
 
 var oz = prompt("How much does it weigh in oz?");
 
-alert(converter(oz));
+alert(oz + " ounces is " + ozToLbs(oz) + " pounds.");
 
 var literToGallon = function(liter) {
   return (liter * 0.264172);
@@ -13,3 +13,26 @@ var literToGallon = function(liter) {
 var liter = prompt("How much does it weigh in liters?");
 
 alert(liter + " liters is " + literToGallon(liter) + " gallons.");
+
+var teaToTable = function(teaspoon) {
+  return (teaspoon * 0.277558);
+};
+
+var teaspoon = prompt("How much is it in teaspoons?");
+
+alert(teaspoon + " teaspoons is " + teaToTable(teaspoon) + " tablespoons.");
+
+jQuery(document).ready(function(){
+jQuery("h1").click(function() {
+  alert("This is a header.");
+});
+
+jQuery("p").click(function() {
+  alert("This is a paragraph.");
+});
+
+jQuery("img").click(function() {
+  alert("This is an image.");
+});
+
+}); //end doc ready
